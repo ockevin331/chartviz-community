e4a7414cc0d04d13114a56bc0ece52174610827b
kevin <ockevin331@gmail.com>
2026-09-02 22:39:55 +0800

    release: prepare ChartViz extension v1.0.13

--- extension/wxt.config.ts
+++ extension/wxt.config.ts
@@ -38,7 +38,7 @@ export function createManifest(): ExtensionManifest {
   return {
     name: 'ChartViz',
     description: 'Chart education in your browser.',
-    version: '1.0.12',
+    version: '1.0.13',
     permissions: [...approvedPermissions],
     host_permissions: [...approvedHostPermissions],
     action: {
